31de4675554ba015a0e8c817159c1e4b70727bfe
aashishrai3799 <aashishrai3799@gmail.com>
2022-06-22 21:04:24 -0400

    Add files via upload

--- texture_model/assets/texture_processing/add_tex.py
+++ texture_model/assets/texture_processing/add_tex.py
@@ -40,11 +40,11 @@ def run_manual():
 				#cv2.waitKey(0) 
 				#cv2.destroyAllWindows()
 
-def add_texture_template(in_path='texture_results/', out_resolution=1024):
+def add_texture_template(in_path='texture_results/', base_path = 'assets/texture_processing/base_tex.npy', out_resolution=1024):
 
 	print('\nAdding Texture Template')
 	
-	base_path = 'assets/texture_processing/base_tex.npy'
+	base_path = base_path
 	base_tex_orig = np.load(base_path)
 	out_path = in_path
 
@@ -66,19 +66,21 @@ def add_texture_template(in_path='texture_results/', out_resolution=1024):
 			
 			for img in imgs:
 
-				in_tex = cv2.imread(os.path.join(in_path,folder,exp,img))
-				#base_tex = cv2.imread(base_path)
-				base_tex = base_tex_orig
+				if 'jpg' in img:
+				
+					in_tex = cv2.imread(os.path.join(in_path,folder,exp,img))
+					#base_tex = cv2.imread(base_path)
+					base_tex = base_tex_orig
 
-				in_tex = cv2.resize(in_tex, (resln-2*percent_crop, resln-2*percent_crop))
-				base_tex = cv2.resize(base_tex, (resln, resln))
+					in_tex = cv2.resize(in_tex, (resln-2*percent_crop, resln-2*percent_crop))
+					base_tex = cv2.resize(base_tex, (resln, resln))
 
-				base_tex[percent_crop-int(resln*0.15):resln-percent_crop-int(resln*0.15),percent_crop:resln-percent_crop,:] = in_tex
+					base_tex[percent_crop-int(resln*0.15):resln-percent_crop-int(resln*0.15),percent_crop:resln-percent_crop,:] = in_tex
 
-				#cv2.imshow('Image', base_tex)
-				cv2.imwrite(os.path.join(out_path,folder,exp,img), base_tex)
+					#cv2.imshow('Image', base_tex)
+					cv2.imwrite(os.path.join(out_path,folder,exp,img), base_tex)
 
-				#cv2.waitKey(0) 
-				#cv2.destroyAllWindows()
+					#cv2.waitKey(0) 
+					#cv2.destroyAllWindows()
 
 
